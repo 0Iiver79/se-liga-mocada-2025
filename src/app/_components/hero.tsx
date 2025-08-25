135dbf604d94cab31bf56d7c96896156750a38b7
Gustavo <gustavovizisbr@gmail.com>
2025-08-24 22:21:41 -0300

    feat: estrutura inicial do projeto com Hero section e imagem responsiva

---
+++ src/app/_components/hero.tsx
@@ -0,0 +1,69 @@
+import { Phone } from "lucide-react";
+import imgHero from "../../../public/imgHero.png";
+import Image from "next/image";
+
+export function Hero() {
+  return (
+    <section className="bg-[#cc9afc] text-white relative overflow-hidden">
+      <div>
+        {/* background responsivo com transparência */}
+        <Image
+          src={imgHero}
+          alt="Imagem de uma mulher olhando para o horizonte"
+          fill
+          sizes="100vw"
+          priority
+          className="object-cover opacity-60 lg:hidden"
+        ></Image>
+      </div>
+      <div className="container mx-auto pt-16 pb-16 md:pb-0 px-4 relative">
+        <article className="grid grid-cols-1 lg:grid-cols-2 gap-8 ">
+          <div className="space-y-4">
+        
+            <h1 className="text-4xl md:text-5xl font-bold leading-tight mb-4">
+              Se Liga Moçada
+            </h1>
+
+            {/* Subtexto */}
+            <p className="text-lg md:text-xl mb-8">
+              Se liga, você não está sozinha. <br />
+              Informação e apoio contra relacionamentos abusivos.
+            </p>
+
+            {/* Botões (CTAs) */}
+            <div className="flex flex-col sm:flex-row gap-4 justify-center md:justify-start">
+              {/* Botão Saiba Mais */}
+              <a
+                href="#"
+                className="bg-[#8C4FA3] px-5 py-2 rounded-md font-semibold flex items-center justify-center w-fit"
+              >
+                Saiba Mais
+              </a>
+
+              {/* Botão Denuncie com ícone */}
+              <a
+                href="#"
+                className="bg-[#8C4FA3] px-5 py-2 rounded-md font-semibold flex items-center justify-center gap-2 w-fit"
+              >
+                <Phone className="text-white w-4 h-4" />
+                Denuncie
+              </a>
+            </div>
+          </div>
+
+          <div className="hidden md:block h-full relative">
+            <Image
+              src={imgHero}
+              alt="Imagem de uma mulher olhando para o horizonte"
+              className="object-contain "
+              fill
+              sizes="(max-width: 768px) 0vw, 50vw"
+              quality={100}
+              priority
+            />
+          </div>
+        </article>
+      </div>
+    </section>
+  );
+}
